2569f8f0396c417eb87a66f453366dd046f69720
ANDREY_KETSKO <ketsman89@gmail.com>
2023-06-21 14:39:13 +0300

    at home

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,3 +1,30 @@
+from typing import Any, Optional
+from django.db import models
 from django.shortcuts import render
 
+from django.views.generic import DetailView
+
+from . import models
+
 # Create your views here.
+
+class CartDetailView(DetailView):
+    template_name = "orders/cart.html"
+    model = models.Cart
+
+    def get_object(self, *args, **kwargs):
+        print(self.request.GET.get("good_id"))
+        print(self.request.GET.get("quantity"))
+        pk = self.request.session.get("cart_id")
+        customer = self.request.user
+        if customer.is_anonymous:
+            customer = None
+        cart, created = models.Cart.objects.get_or_create(
+            pk=pk,
+            defaults={
+                "customer": customer
+            }
+        )
+        if created:
+            self.request.session['cart_id'] = cart.pk
+        return cart
--- src/proj/urls.py
+++ src/proj/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_views.HomePage.as_view(), name="home-page"),
     path('spravochniki/', include('spravochniki.urls', namespace='spravochniki')),  
-    path('staff/', include('staff.urls', namespace='staff'))
+    path('staff/', include('staff.urls', namespace='orders')),
+    path('orders/', include('orders.urls', namespace='staff'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Этого не должно быть в продакшене, поэтому плючуем только в дев сервере,
